356c4e189a72cc86aa612ac5216454126681072b
arkhyion <91257378+arkhyion@users.noreply.github.com>
2021-09-23 13:14:16 +0200

    ajout lecture d'un fichier

--- CODE/src/Main.java
+++ CODE/src/Main.java
@@ -1,11 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		System.out.println("coucou");
-		DBParams.DBPath = "";
+		DBParams.DBPath = "../../DB/";
 		DBParams.PageSize = 4096;
 		DBParams.maxPagesPerFile = 4;
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("../../DB/test.txt"));
+			String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+		}catch(FileNotFoundException e){
+			System.out.println("Erreur, fichier introuvable");
+			e.getMessage();
+		}catch(IOException e) {
+			e.getMessage();
+		}
 	}
-
 }

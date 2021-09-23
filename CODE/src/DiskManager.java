e9956da5eadc6f6725c6a3244224d11812c94fbc
Zectalier <91257712+Zectalier@users.noreply.github.com>
2021-09-23 13:48:04 +0200

    Ajout squelette de DiskManager.java

---
+++ CODE/src/DiskManager.java
@@ -0,0 +1,37 @@
+import java.nio.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class DiskManager {
+
+	public PageID AllocPage() {
+		
+		boolean found = false;
+		int x = 0;
+		int pageid = 0;
+		String filename = String.format("../DB/f%d.df",x);
+		File file = new File(filename);
+		
+		if (!file.exists()) {
+			//Creer le fichier f0x.df ici
+		}
+		
+		while(found == false) {
+			filename = String.format("../DB/f%d.df",x);
+			file = new File(filename);
+			long fileSize = file.length();
+			if(fileSize <= DBParams.PageSize * DBParams.maxPagesPerFile) {
+				
+			}
+		}
+	}
+	
+	public void ReadPage(int pageId,ByteBuffer buff) {
+		
+	}
+	
+	public void WritePage(int pageId,ByteBuffer[] buff) {
+		
+	}
+}

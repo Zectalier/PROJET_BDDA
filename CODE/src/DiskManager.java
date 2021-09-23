0918e44eddd4285b844a66a21cbd03f12d706077
Zectalier <91257712+Zectalier@users.noreply.github.com>
2021-09-23 14:03:08 +0200

    Update DiskManager.java + Ajout de commentaires

--- CODE/src/DiskManager.java
+++ CODE/src/DiskManager.java
@@ -1,7 +1,5 @@
 import java.nio.*;
 import java.io.File;
-import java.io.IOException;
-import java.util.*;
 
 public class DiskManager {
 
@@ -9,22 +7,39 @@ public class DiskManager {
 		
 		boolean found = false;
 		int x = 0;
-		int pageid = 0;
+		PageID tmp = new PageID();
 		String filename = String.format("../DB/f%d.df",x);
 		File file = new File(filename);
 		
+		//Si le fichier f0.df n'existe pas
 		if (!file.exists()) {
-			//Creer le fichier f0.df ici
+			//Creer le nouveau fichier f0.df ici
 		}
 		
+		//Recherche d'un fichier non plein
 		while(found == false) {
 			filename = String.format("../DB/f%d.df",x);
 			file = new File(filename);
 			long fileSize = file.length();
+			
+			//Si le fichier a de l'espace disponible
 			if(fileSize <= DBParams.PageSize * DBParams.maxPagesPerFile) {
+				//Allouer la page ici
 				
+				found = true;
 			}
+			
+			//Si on ne trouve pas de fichiers qui a de la place
+			else if (!file.exists()) {
+				//Creer le nouveau fichier fx.df ici
+				return tmp; //retourne un PageID ( lequel? )
+			}
+			
+			//Si fx.df ne convient pas, on cherche pour x+1;
+			x++;
 		}
+		
+		return tmp; //retourne un PageID ( lequel? )
 	}
 	
 	public void ReadPage(int pageId,ByteBuffer buff) {
@@ -35,3 +50,4 @@ public class DiskManager {
 		
 	}
 }
+

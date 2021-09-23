508254dd5a698d4d529e6528b71e4735b5054c75
arkhyion <91257378+arkhyion@users.noreply.github.com>
2021-09-23 13:37:39 +0200

    suppression test lecture fichier

--- CODE/src/Main.java
+++ CODE/src/Main.java
@@ -12,18 +12,5 @@ public class Main {
 		DBParams.PageSize = 4096;
 		DBParams.maxPagesPerFile = 4;
 		
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader("../../DB/test.txt"));
-			String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-            reader.close();
-		}catch(FileNotFoundException e){
-			System.out.println("Erreur, fichier introuvable");
-			e.getMessage();
-		}catch(IOException e) {
-			e.getMessage();
-		}
 	}
 }

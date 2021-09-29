1d715976527f6b11de9773b0b4ae536859930c68
Zectalier <91257712+Zectalier@users.noreply.github.com>
2021-09-29 14:43:19 +0200

    Add files via upload

---
+++ CODE/src/DiskManagerTests.java
@@ -0,0 +1,25 @@
+import java.nio.file.Path;
+import java.io.File;
+import java.nio.*;
+import java.nio.charset.StandardCharsets;
+
+public class DiskManagerTests {
+
+	public static void main(String[] args) {
+		
+		//Tests à faire si possible sur un dossier DB vide
+		
+		DBParams.DBPath = "../DB/";
+		DBParams.PageSize = 4096;
+		DBParams.maxPagesPerFile = 4;
+		DBParams.frameCount = 2;
+		
+		PageID page = new PageID();
+		
+		//Test allocation nouvelle page
+		page = DiskManager.AllocPage();
+		
+		ByteBuffer buffer = ByteBuffer.allocate(DBParams.PageSize);
+		
+	}
+}

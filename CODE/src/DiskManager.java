770468c18d892cd461276fd572cd0652a9ff0758
Zectalier <91257712+Zectalier@users.noreply.github.com>
2021-10-04 02:05:26 +0200

    Suppresion code pour si pas de fichier dans dossier
    Code concernant un répertoire vide inutile car la vérification de si un fichier existe est fait dans la boucle while(!found) + Optimisation du code

--- CODE/src/DiskManager.java
+++ CODE/src/DiskManager.java
@@ -10,33 +10,18 @@ public class DiskManager {
 		
 		boolean found = false;
 		int x = 0;
-		String filename = String.format(DBParams.DBPath+"f%d.df",x);
-		File file = new File(filename);
-		PageID page = new PageID(0,0);
+		String filename;
+		File file;
+		PageID page = new PageID();
 		
-		//Si le fichier f0.df n'existe pas
-		if (!file.exists()) {
-			//Creer le nouveau fichier f0.df ici
-			try{
-				file.createNewFile();
-			}catch(IOException e) {
-				System.out.println("Erreur, le fichier n'a pas pu être crée");
-				e.getMessage();
-			}
-			
-			//Allouer la page PageID(0,0) au nouveau fichier ( qui devrait être "f0.df" ) avec WritePage()
-			page = new PageID(x,0);
-			ByteBuffer buffer = ByteBuffer.allocate(DBParams.PageSize);
-			for(int i = 0; i<DBParams.PageSize;i++) {
-				buffer.put(i,(byte)0);
-			}
-			WritePage(page,buffer);
-			found = true;
-			return page; //retourne un PageID (ici normalement PageID(0,0)
+		//Création du buffer à mettre dans la page lors de l'allocation
+		ByteBuffer buffer = ByteBuffer.allocate(DBParams.PageSize);
+		for(int i = 0; i<DBParams.PageSize;i++) {
+			buffer.put(i,(byte)0);
 		}
 		
 		//Recherche d'un fichier non plein
-		while(found == false) {
+		while(!found) {
 			filename = String.format(DBParams.DBPath+"f%d.df",x);
 			file = new File(filename);
 			
@@ -52,10 +37,6 @@ public class DiskManager {
 				}
 				//Allouer la page PageID(x,0) au nouveau fichier ( qui devrait être fx.df ) avec WtitePage()
 				page = new PageID(x,0);
-				ByteBuffer buffer = ByteBuffer.allocate(DBParams.PageSize);
-				for(int i = 0; i<DBParams.PageSize;i++) {
-					buffer.put(i,(byte)0);
-				}
 				WritePage(page,buffer);
 				found = true;
 				return page; //retourne un PageID (ici normalement PageID(x,0))
@@ -72,17 +53,12 @@ public class DiskManager {
 				page = new PageID(x,n);
 				
 				//Allouer la page PageID(x,i) ici avec WritePage()
-				ByteBuffer buffer = ByteBuffer.allocate(DBParams.PageSize);
-				for(int i = 0; i<DBParams.PageSize;i++) {
-					buffer.put(i,(byte)0);
-				}
 				WritePage(page,buffer);
 				found = true;
 			}
 			//Si fx.df ne convient pas, on cherche pour x+1;
 			x++;
 		}
-		
 		return page; //Devrait retourner le pageID trouvé si un fichier non vide existe dans la boucle précédente
 	}
 

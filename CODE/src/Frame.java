0829f8af1ca2f89468b4f47e73a7250d42dc6d03
Zectalier <91257712+Zectalier@users.noreply.github.com>
2021-10-06 15:12:25 +0200

    Update Frame.java
    Rajout des méthodes afin de pouvoir accéder aux variables

--- CODE/src/Frame.java
+++ CODE/src/Frame.java
@@ -5,6 +5,53 @@ public class Frame {
 	private ByteBuffer buffer;
 	private PageID pageId;
 	private int pinCount;
-	private boolean flag;
+	private boolean dirty;
+	
+	public Frame(PageID pageId) {
+		this.pinCount = 1;
+		this.dirty = false;
+		this.pageId = pageId;
+		buffer = ByteBuffer.allocate(DBParams.PageSize);
+		DiskManager.ReadPage(pageId,buffer);
+	}
+
+	public Frame() {
+		this.pinCount = 0;
+		this.dirty = false;
+		this.pageId = null;
+		buffer = null;
+	}
+	
+	public ByteBuffer getBuffer() {
+		return buffer;
+	}
+
+	public void setBuffer(ByteBuffer buffer) {
+		this.buffer = buffer;
+	}
+
+	public PageID getPageId() {
+		return pageId;
+	}
+
+	public void setPageId(PageID pageId) {
+		this.pageId = pageId;
+	}
+
+	public int getPinCount() {
+		return pinCount;
+	}
+
+	public void setPinCount(int pinCount) {
+		this.pinCount = pinCount;
+	}
+
+	public boolean isDirty() {
+		return dirty;
+	}
+
+	public void setDirty(boolean dirty) {
+		this.dirty = dirty;
+	}
 	
 }

e038ff295de887cd03ad388797e3e7ab003c3bbd
benjo27 <91257741+benjo27@users.noreply.github.com>
2021-10-13 15:31:18 +0200

    catalog modif

--- CODE/src/Catalog.java
+++ CODE/src/Catalog.java
@@ -1,21 +1,53 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Catalog {
 	
 	private ArrayList<RelationInfo> tableau_rel_info;
-	private int compteur;
+
 	
-	public Catalog(ArrayList<RelationInfo>tab, int c) {
-		ArrayList<RelationInfo> tableau_rel_info = tab;
-		compteur = c;
+	public Catalog(ArrayList<RelationInfo>tab) {
+		tableau_rel_info = tab;
 	}
 	
 	public void Init() {
-		
+		try{
+			FileInputStream tab = new FileInputStream(DBParams.DBPath+"Catalog.def");
+			ObjectInputStream object =  new ObjectInputStream(tab) ;
+			
+			// cette ligne ne marche pas, a completer 
+		    tableau_rel_info = (ArrayList<RelationInfo>) object.readObject();
+
+		    object.close();
+			
+		}catch(IOException e) {
+			System.out.println("Erreur, le fichier n'a pas pu être crée");
+			e.getMessage();
+		}catch(ClassNotFoundException e) {
+			System.out.println("Erreur, classe inexistante");
+			e.getMessage();
+		}
 	}
-	
+
 	public void Finish() {
 		
+		try{
+			FileOutputStream tab = new FileOutputStream(DBParams.DBPath+"Catalog.def");
+			ObjectOutputStream object =  new ObjectOutputStream(tab) ;
+			
+			object.writeObject(tableau_rel_info);
+			object.close();
+			
+		}catch(IOException e) {
+			System.out.println("Erreur, le fichier n'a pas pu être crée");
+			e.getMessage();
+		}
 	}
 	
 	public void AddRelation(RelationInfo liste) {

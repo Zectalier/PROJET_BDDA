13cdcde49b3b1beb147821b56f537d5e3c8ae446
arkhyion <91257378+arkhyion@users.noreply.github.com>
2021-11-10 15:19:41 +0100

    update Catalog.java

--- CODE/src/Catalog.java
+++ CODE/src/Catalog.java
@@ -1,35 +1,31 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
-public class Catalog {
+public enum Catalog {
+	INSTANCE;
 
 	private ArrayList<RelationInfo> tableau_rel_info;
 
-
-	public Catalog(ArrayList<RelationInfo>tab) {
-		tableau_rel_info = tab;
+	private Catalog() {
+		tableau_rel_info = new ArrayList<RelationInfo>();
 	}
 
 	public void Init() {
-		try{
-			FileInputStream tab = new FileInputStream(DBParams.DBPath+"Catalog.def");
-			ObjectInputStream object =  new ObjectInputStream(tab) ;
-
-			// cette ligne ne marche pas, a completer 
-			tableau_rel_info = (ArrayList<RelationInfo>) object.readObject();
-
+		try {
+			FileInputStream tab = new FileInputStream(new File(DBParams.DBPath + "Catalog.def"));
+			ObjectInputStream object = new ObjectInputStream(tab);
+			tableau_rel_info= (ArrayList<RelationInfo>) (object.readObject());
 			object.close();
 
-		}catch(IOException e) {
-			System.out.println("Erreur, le fichier n'a pas pu être crée");
+		} catch (IOException e) {
+			System.out.println("Erreur, le fichier n'a pas pu Ãªtre trouvÃ©e");
 			e.getMessage();
-		}catch(ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			System.out.println("Erreur, classe inexistante");
 			e.getMessage();
 		}
@@ -37,15 +33,14 @@ public class Catalog {
 
 	public void Finish() {
 
-		try{
-			FileOutputStream tab = new FileOutputStream(DBParams.DBPath+"Catalog.def");
-			ObjectOutputStream object =  new ObjectOutputStream(tab) ;
-
+		try {
+			FileOutputStream tab = new FileOutputStream(new File(DBParams.DBPath + "Catalog.def"));
+			ObjectOutputStream object = new ObjectOutputStream(tab);
 			object.writeObject(tableau_rel_info);
 			object.close();
 
-		}catch(IOException e) {
-			System.out.println("Erreur, le fichier n'a pas pu être crée");
+		} catch (IOException e) {
+			System.out.println("Erreur, le fichier n'a pas pu Ãªtre trouvÃ©e");
 			e.getMessage();
 		}
 	}
@@ -53,4 +48,5 @@ public class Catalog {
 	public void AddRelation(RelationInfo liste) {
 		tableau_rel_info.add(liste);
 	}
+
 }

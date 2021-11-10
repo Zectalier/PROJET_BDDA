4ab32b338a84b4e5e6abdfade81abad72dc44577
arkhyion <darksyder750@gmail.com>
2021-11-10 15:06:54 +0100

    Update Catalog + ajout TestCatalog

--- CODE/src/Catalog.java
+++ CODE/src/Catalog.java
@@ -1,33 +1,31 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
-public class Catalog {
+public enum Catalog {
+	INSTANCE;
 	
 	private ArrayList<RelationInfo> tableau_rel_info;
 
 	
-	public Catalog(ArrayList<RelationInfo>tab) {
-		tableau_rel_info = tab;
+	private Catalog() {
+		tableau_rel_info = new ArrayList<RelationInfo>();
 	}
 	
 	public void Init() {
 		try{
-			FileInputStream tab = new FileInputStream(DBParams.DBPath+"Catalog.def");
+			FileInputStream tab = new FileInputStream(new File(DBParams.DBPath+"Catalog.def"));
 			ObjectInputStream object =  new ObjectInputStream(tab) ;
-			
-			// cette ligne ne marche pas, a completer 
-		    tableau_rel_info = (ArrayList<RelationInfo>) object.readObject();
-
+			ArrayList<RelationInfo> tabl= (ArrayList<RelationInfo>) (object.readObject());
+			setTabRelInfo(tabl);
 		    object.close();
 			
 		}catch(IOException e) {
-			System.out.println("Erreur, le fichier n'a pas pu être crée");
+			System.out.println("Erreur, le fichier n'a pas pu être trouvée");
 			e.getMessage();
 		}catch(ClassNotFoundException e) {
 			System.out.println("Erreur, classe inexistante");
@@ -38,19 +36,21 @@ public class Catalog {
 	public void Finish() {
 		
 		try{
-			FileOutputStream tab = new FileOutputStream(DBParams.DBPath+"Catalog.def");
+			FileOutputStream tab = new FileOutputStream(new File(DBParams.DBPath+"Catalog.def"));
 			ObjectOutputStream object =  new ObjectOutputStream(tab) ;
-			
 			object.writeObject(tableau_rel_info);
 			object.close();
 			
 		}catch(IOException e) {
-			System.out.println("Erreur, le fichier n'a pas pu être crée");
+			System.out.println("Erreur, le fichier n'a pas pu être trouvée");
 			e.getMessage();
 		}
 	}
 	
 	public void AddRelation(RelationInfo liste) {
 		tableau_rel_info.add(liste);
+	}
+	public void setTabRelInfo(ArrayList<RelationInfo> tab) {
+		tableau_rel_info=tab;
 	}
 }

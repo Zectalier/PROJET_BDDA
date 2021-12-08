14b6581a429eb2a44c0b332ab0d9cb5190844517
benjo27 <91257741+benjo27@users.noreply.github.com>
2021-12-08 12:46:04 +0100

    Update Catalog.java

--- CODE/src/Catalog.java
+++ CODE/src/Catalog.java
@@ -7,7 +7,6 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public enum Catalog {
-	
 	INSTANCE;
 
 	private ArrayList<RelationInfo> tableau_rel_info;
@@ -15,13 +14,16 @@ public enum Catalog {
 	private Catalog() {
 		tableau_rel_info = new ArrayList<RelationInfo>();
 	}
-
+	
+	// i add line 19 cause i got a warning on line 24
+	@SuppressWarnings("unchecked")
 	public void Init() {
 		try {
 			FileInputStream tab = new FileInputStream(new File(DBParams.DBPath + "Catalog.def"));
 			ObjectInputStream object = new ObjectInputStream(tab);
 			tableau_rel_info= (ArrayList<RelationInfo>) (object.readObject());
 			object.close();
+
 		} catch (IOException e) {
 			System.out.println("Erreur, le fichier n'a pas pu être trouvée");
 			e.getMessage();
@@ -50,7 +52,7 @@ public enum Catalog {
 		}
 		tableau_rel_info.add(liste);
 	}
-
+	
 	public void reset() {
 		tableau_rel_info = new ArrayList<RelationInfo>();
 		File catalogFile = new File(DBParams.DBPath + "Catalog.def");

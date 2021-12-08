2b9338fd716e02a133b2f19da266f0c29eb952f8
benjo27 <91257741+benjo27@users.noreply.github.com>
2021-12-08 13:13:47 +0100

    Create CatalogTests.java

---
+++ CODE/src/CatalogTests.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class CatalogTests {	
+  public static void main(String[]args) {
+  
+		DBParams.DBPath = "../DB/";
+		DBParams.PageSize = 4096;
+		DBParams.maxPagesPerFile = 4;
+		DBParams.frameCount = 2;
+		
+		RelationInfo relInfo = new RelationInfo("test",0,new ArrayList<ColInfo>(),new PageID(0,0));
+		
+		Catalog.INSTANCE.Init();
+		Catalog.INSTANCE.AddRelation(relInfo);
+		Catalog.INSTANCE.Finish();
+	}
+}
